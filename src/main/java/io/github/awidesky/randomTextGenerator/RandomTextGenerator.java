d9bf8c0b5d03a0670529f2e6550a7878b106846a
Eugene Hong <awidesky@naver.com>
2023-08-06 21:53:00 +0900

    add comments & deploy code for pom.xml

--- src/main/java/io/github/awidesky/randomTextGenerator/RandomTextGenerator.java
+++ src/main/java/io/github/awidesky/randomTextGenerator/RandomTextGenerator.java
@@ -3,20 +3,38 @@ package io.github.awidesky.randomTextGenerator;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * A Random text generator.
+ * */
 public class RandomTextGenerator {
 
 	private final UnicodeLanguage[] ranges;
 	
-	public RandomTextGenerator(UnicodeLanguage... availableRanges) {
-		ranges = availableRanges;
+	/**
+	 * Creates a random text generator that uses given languages.
+	 * */
+	public RandomTextGenerator(UnicodeLanguage... launguages) {
+		ranges = launguages;
 	}
 	
+	/**
+	 * Generates a random {@code String} in given length.
+	 * Each candidate character has same possibility of occurrence.
+	 * So if one of the selected language(like Korean, Chinese, etc..) has way more characters than the others(like English),
+	 * characters of the language will occur in result text way more than characters of other languages.
+	 * */
 	public String randomString(int len) {
 		StringBuilder sb = new StringBuilder(len);
 		new Random().ints(0, 1114111).filter(i -> Arrays.stream(ranges).anyMatch(u -> u.test(i))).limit(len).mapToObj(Character::toChars).forEach(sb::append);
 		return sb.toString();
 	}
 	
+	/**
+	 * Generates a <i>evenly-distributed</i> random {@code String} in given length.
+	 * Each candidate language has same possibility of occurrence.
+	 * So even if one of the selected language(like Korean, Chinese, etc..) has way more characters than the others(like English),
+	 * characters of the language will have same frequency of occurrence with characters of other languages.
+	 * */
 	public String evenlyDistributedRandomString(int len) {
 		StringBuilder sb = new StringBuilder(len);
 		new Random().ints(len, 0, Integer.MAX_VALUE).map(i -> i % ranges.length).flatMap(i -> 
--- src/main/java/io/github/awidesky/randomTextGenerator/UnicodeLanguage.java
+++ src/main/java/io/github/awidesky/randomTextGenerator/UnicodeLanguage.java
@@ -1,30 +1,47 @@
 package io.github.awidesky.randomTextGenerator;
 
 import java.util.Arrays;
-import java.util.function.Predicate;
+import java.util.function.IntPredicate;
 
-public class UnicodeLanguage implements Predicate<Integer> {
+/**
+ * Represent set of unicode characters (languages, symbols, etc...).
+ * If needed unicode characters are not in static fields, you can create one.
+ * <a href="https://en.wikipedia.org/wiki/List_of_Unicode_characters">https://en.wikipedia.org/wiki/List_of_Unicode_characters</a> 
+ * */
+public class UnicodeLanguage implements IntPredicate {
 
-	//TODO : Better Test method..?
+	/** ASCII numeric characters(0~9) */
 	public static final UnicodeLanguage NUMERIC = new UnicodeLanguage(48, 57);
+	/** ASCII English alphabets(both lower case and upper case) */
 	public static final UnicodeLanguage ENGLISH = new UnicodeLanguage(new Range(65, 90), new Range(97, 122));
+	/** ASCII punctuation & symbols */
 	public static final UnicodeLanguage ASCII_PUNCTUATION_SYMBOL = new UnicodeLanguage(new Range(33, 47), new Range(58, 64), new Range(91, 96), new Range(123, 126));
+	/** Unicode Korean (한글 소리 마디/Hangul Syllables) */
 	public static final UnicodeLanguage KOREAN = new UnicodeLanguage(new Range(44032, 55203));
 	
 	private final Range[] rangeArr;
 
 	/**
-	 * Both parameters are <i>inclusive</i>.
+	 * Creates an set of unicode characters.
+	 * unicode characters whose code point is between {@code unicodePointStart} and {@code unicodePointLast} are included.
+	 * 
+	 * @param unicodePointStart <i>inclusive</i>
+	 * @param unicodePointLast <i>inclusive</i>
 	 * */
 	public UnicodeLanguage(int unicodePointStart, int unicodePointLast) {
 		rangeArr = new Range[] {new Range(unicodePointStart, unicodePointLast)};
 	}
+	/**
+	 * Creates an set of unicode characters.
+	 * unicode characters whose code point is in given {@code Range} are included.
+	 */
 	public UnicodeLanguage(Range... ranges) {
 		rangeArr = ranges;
 	}
 
+	/** Test if given unicode code point is included in this {@code UnicodeLanguage} instance. */
 	@Override
-	public boolean test(Integer t) {
+	public boolean test(int t) {
 		return Arrays.stream(rangeArr).anyMatch(r -> r.check(t));
 	}
 	
@@ -33,8 +50,9 @@ public class UnicodeLanguage implements Predicate<Integer> {
 		public final int unicodePointLast;
 		
 		/**
-		 * Both parameters are <i>inclusive</i>.
-		 * */
+		 * @param unicodePointStart <i>inclusive</i>
+		 * @param unicodePointLast  <i>inclusive</i>
+		 */
 		public Range(int unicodePointStart, int unicodePointLast) {
 			this.unicodePointStart = unicodePointStart;
 			this.unicodePointLast = unicodePointLast;
